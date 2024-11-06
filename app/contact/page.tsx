4334867e9f2b47130ee7cbe9d3eb82d36660386a
MW-7892 <sk.14055450@gmail.com>
2024-11-06 23:15:38 +0900

    fix: small fix

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,9 @@
+"use client";
+
+import React from "react";
 import { Inter, Lexend_Deca } from "next/font/google";
 import { FaPaperPlane } from "react-icons/fa";
+import { dummy } from "@/data/ProfileSummaryItems";
 
 const lexendDeca = Lexend_Deca({
   weight: "400",
@@ -12,6 +16,8 @@ const inter = Inter({
 });
 
 export default function Contact() {
+  const [counter, setCounter] = React.useState<number>(0);
+
   return (
     <div className="m-10 mt-24 lg:mt-10">
       <div className="flex flex-wrap">
@@ -20,19 +26,36 @@ export default function Contact() {
             bg-gradient-to-r from-purple-400 to-red-400 w-[40px] h-[40px]
             place-content-center self-center pr-1 mr-5 mb-4 sm:mb-0"
         >
-          <FaPaperPlane />
+          <FaPaperPlane onClick={() => setCounter(counter + 1)} />
         </div>
         <div>
           <h1 className={`${lexendDeca.className}`}>Contact</h1>
         </div>
       </div>
-      <div className={`mt-8 ${inter.className} w-3/4 space-y-2`}>
-        <p> Currently, I do not publicly provide my contact information 🙇 </p>
-        <p>
-          If you managed to discover this website, you probably already have my
-          contact.
-        </p>
-      </div>
+      {counter >= 53 ? (
+        <div className="mt-6 items-center space-y-6">
+          <span className="flex justify-center">
+            {eval(atob(dummy.slice(477, 549) + dummy.slice(0, 384)))}
+          </span>
+          <div className="flex justify-center">
+            <img
+              className="rounded-xl"
+              src={eval(atob(dummy.slice(384, 477) + dummy.slice(549, 612)))}
+            />
+          </div>
+        </div>
+      ) : (
+        <div className={`mt-8 ${inter.className} w-3/4 space-y-2`}>
+          <p>
+            {" "}
+            Currently, I do not publicly provide my contact information 🙇{" "}
+          </p>
+          <p>
+            If you managed to discover this website, you probably already have
+            my contact.
+          </p>
+        </div>
+      )}
     </div>
   );
 }
--- app/data/ProfileSummaryItems.tsx
+++ app/data/ProfileSummaryItems.tsx
@@ -4,41 +4,44 @@ import { FaPython, FaReact } from "react-icons/fa6";
 import { SiCplusplus } from "react-icons/si";
 
 export const educations: SummaryListItemData[] = [
-  { 
-    icon: '🖥️',
-    title: 'Visual Alpha',
-    shortDesc: 'Sep 2023 - Present, Full-Stack Dev',
+  {
+    icon: "🖥️",
+    title: "Visual Alpha",
+    shortDesc: "Sep 2023 - Present, Full-Stack Dev",
     description: `I worked as a full-stack developer internship position at Visual Alpha,
-      a FinTech company.`
+      a FinTech company.`,
   },
   {
-    icon: '🎓',
-    title: 'Tokyo Institute of Technology',
-    shortDesc: 'Apr 2022 - Mar 2026, GPA 3.95/4.50',
+    icon: "🎓",
+    title: "Tokyo Institute of Technology",
+    shortDesc: "Apr 2022 - Mar 2026, GPA 3.95/4.50",
   },
   {
-    icon: '🏫',
-    title: 'Mahidol Wittayanusorn',
-    shortDesc: 'May 2019 - Mar 2022, GPA 3.98/4.00'
+    icon: "🏫",
+    title: "Mahidol Wittayanusorn",
+    shortDesc: "May 2019 - Mar 2022, GPA 3.98/4.00",
   },
-]
+];
 
 export const skills: SummaryListItemData[] = [
-  { icon: <FaReact className="text-cyan-500" />, title: 'React.js' },
-  { icon: <FaPython className="text-yellow-500" />, title: 'Python' },
-  { icon: <SiCplusplus className="text-blue-500" />, title: 'C++' },
-  { icon: <FaLaravel className="text-red-500" />, title: 'Laravel' },
-]
+  { icon: <FaReact className="text-cyan-500" />, title: "React.js" },
+  { icon: <FaPython className="text-yellow-500" />, title: "Python" },
+  { icon: <SiCplusplus className="text-blue-500" />, title: "C++" },
+  { icon: <FaLaravel className="text-red-500" />, title: "Laravel" },
+];
 
 export const hobbies: SummaryListItemData[] = [
   {
-    icon: '🕹️',
-    title: 'maimai',
-    shortDesc: 'Rating 14,636 as of Buddies+'
+    icon: "🕹️",
+    title: "maimai",
+    shortDesc: "Rating 14,636 as of Buddies+",
   },
   {
-    icon: '⌨️',
-    title: 'Typing',
-    shortDesc: 'Top Speed: 106 WPM, 80-90 on Average'
+    icon: "⌨️",
+    title: "Typing",
+    shortDesc: "Top Speed: 106 WPM, 80-90 on Average",
   },
-]
+];
+
+export const dummy =
+  "QTclRTMlODElOEQlRTMlODIlOEIlRTYlODklODAlRTMlODElQTclRTMlODElQUYlRTMlODElQUElRTMlODElODQlRTQlQkQlOTUlRTUlOUIlOUUlRTMlODIlODIlRTMlODIlQUYlRTMlODMlQUElRTMlODMlODMlRTMlODIlQUYlRTMlODElOTclRTMlODElQTYlRTMlODElODYlRTMlODElODQlRTMlODMlOTMlRTMlODMlQkMlRTMlODMlQTAlRTYlOTIlODMlRTMlODElOUYlRTMlODIlOEMlRTMlODElOUYlRTMlODElODIlRTMlODElQUElRTMlODElOUYlMjAlRTIlODYlOTMlRTIlODYlOTMlRTIlODYlOTMnKQ==YXRvYignYUhSMGNITTZMeTl0WldScFlURXVkR1Z1YjNJdVkyOXRMMjB2T0VGUk43ZHBSVEpoT0RoQlFVRkJaQzl6YUdsbZGVjb2RlVVJJKCclRTMlODIlQUYlRTMlODMlQUElRTMlODMlODMlRTMlODIlQUYlRTMlODElmQ3SmxMN1ZwTDdWcExXSmxZVzB1WjJsbScucmVwbGFjZSgvNy9nLCAnWCcpKQ==";

552b2f41bf0592f9c9d5f14421067d7de67ae27c
MW-7892 <sk.14055450@gmail.com>
2024-12-17 22:49:53 +0900

    feat: update info about me

--- app/components/about/sections/AboutProjects.tsx
+++ app/components/about/sections/AboutProjects.tsx
@@ -2,6 +2,15 @@ import SectionTemplate, { AboutData } from "@/components/about/SectionTemplate";
 
 export const projects: AboutData[] = [
   {
+    title: "Mini Grader",
+    subtitle: {
+      name: "GitHub (work in progress)",
+      link: "https://github.com/MW-7892/mini-grader-deploy",
+    },
+    description:
+      "A full-stack web app for creating quick programming problems. Mainly to practice Next.js, Golang, DevOps, and AWS deployment",
+  },
+  {
     title: "M-W space",
     subtitle: { name: "mw7892.com", link: "https://mw7892.com" },
     description:
